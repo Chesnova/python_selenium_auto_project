54a2e5f446158e048d2dc1c361081837acfcfbfd
Chesnova <myjobistester@gmail.com>
2022-05-13 22:17:55 +0300

    add new metods and locators

--- pages/locators.py
+++ pages/locators.py
@@ -5,3 +5,14 @@ class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_FORM = (By.ID, "login_form")
+    LOGIN_REGISTER_FORM = (By.ID, "register_form")
+
+    LOGIN_USERNAME = (By.ID, "id_login-username")
+    LOGIN_PASSWORD = (By.ID, "id_login-password")
+    LOGIN_SUBMIT = (By.NAME, "login_submit")
+
+    LOGIN_REGISTRATION_EMAIL = (By.ID, "id_registration-email")
+    LOGIN_REGISTRATION_PASSWORD1 = (By.ID, "id_registration-password1")
+    LOGIN_REGISTRATION_PASSWORD2 = (By.ID, "id_registration-password2")
+    LOGIN_REGISTRATION_SUBMIT = (By.NAME, "registration_submit")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,12 +9,16 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
+        url = self.browser.current_url
+        assert url, "login url is not presented"
         assert True
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form is not presented"
         assert True
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER_FORM), "register form is not presented"
         assert True
